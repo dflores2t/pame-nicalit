373ea990f596de8d073d7fbda687e25e2f130881
Daniel Flores <dflores2t@gmail.com>
2023-01-04 08:17:30 -0600

    removed social media section from footer and removed text-decoration from copyright anchor and added hover color

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,53 +1,11 @@
 <template>
   <footer class="mt-5">
-    <!-- Grid container -->
-
-    <!-- Section: Social media -->
-    <section class="social-media">
-      <!-- Facebook -->
-      <a
-        class="btn btn-link btn-floating btn-lg m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fa-brands fa-facebook-f"></i
-      ></a>
-
-      <!-- Twitter -->
-      <a
-        class="btn btn-link btn-floating btn-lg m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-twitter"></i
-      ></a>
-
-      <!-- Instagram -->
-      <a
-        class="btn btn-link btn-floating btn-lg m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-instagram"></i
-      ></a>
-
-      <!-- Linkedin -->
-      <a
-        class="btn btn-link btn-floating btn-lg m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        ><i class="fab fa-linkedin"></i
-      ></a>
-    </section>
-    <!-- Section: Social media -->
-
-    <!-- Grid container -->
-
     <!-- Copyright -->
     <div class="copyright">
-      ©2022 Copyright: PAME by
-      <a href="https://propositivpr.com/">propositivapr.com</a>
+      <i class="fa-regular fa-copyright"></i>2022 PAME .::. Developed by
+      <a href="https://www.propositivapr.com/" target="_blank"
+        >propositivapr.com</a
+      >
     </div>
     <!-- Copyright -->
   </footer>
@@ -66,10 +24,14 @@ footer {
   left: 0;
   right: 0;
 }
-.social-media {
-  background-color: #d8e183;
-}
 .copyright {
   background-color: black;
+}
+.copyright a {
+  text-decoration: none;
+  color: #ffffff;
+}
+.copyright a:hover {
+  color: #fa9100;
 }
 </style>

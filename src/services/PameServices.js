3b09f1367dd80086187b86421439202f12a51c33
Daniel Flores <dflores2t@gmail.com>
2023-03-12 15:45:56 -0600

    added news units

--- src/services/PameServices.js
+++ src/services/PameServices.js
@@ -1,362 +1,367 @@
-import store from "../store";
-import axios from "axios";
-import imageCompression from "browser-image-compression";
-import { SERVER_INFO } from "./ServerInfo";
+import store from '../store'
+import axios from 'axios'
+import imageCompression from 'browser-image-compression'
+import { SERVER_INFO } from './ServerInfo'
 
 export const Departamento = [
-  { text: "MANAGUA", value: "MANAGUA" },
-  { text: "CARAZO", value: "CARAZO" },
-  { text: "MASAYA", value: "MASAYA" },
-];
+  { text: 'MANAGUA', value: 'MANAGUA' },
+  { text: 'CARAZO', value: 'CARAZO' },
+  { text: 'MASAYA', value: 'MASAYA' }
+]
 
 export const Municipios = {
   MANAGUA: [
-    { text: "MANAGUA", value: "MANAGUA" },
-    { text: "SAN RAFAEL DEL SUR", value: "SAN RAFAEL DEL SUR" },
+    { text: 'MANAGUA', value: 'MANAGUA' },
+    { text: 'SAN RAFAEL DEL SUR', value: 'SAN RAFAEL DEL SUR' }
   ],
-  CARAZO: [{ text: "JINOTEPE", value: "JINOTEPE" }],
-  MASAYA: [{ text: "MASATEPE", value: "MASATEPE" }],
-};
+  CARAZO: [{ text: 'JINOTEPE', value: 'JINOTEPE' }],
+  MASAYA: [{ text: 'MASATEPE', value: 'MASATEPE' }]
+}
 export const Comarcas = {
-  MANAGUA: [{ text: "SIN INFORMACIÓN", value: "SIN INFORMACIÓN" }],
-  "SAN RAFAEL DEL SUR": [
-    { text: "SAN RAFAEL DEL SUR", value: "SAN RAFAEL DEL SUR" },
-    { text: "EL SALTO", value: "EL SALTO" },
-    { text: "LOS RIZO", value: "LOS RIZO" },
-    { text: "MASACHAPA", value: "MASACHAPA" },
-    { text: "SAN PABLO", value: "SAN PABLO" },
-    { text: "LOS LARIOS", value: "LOS LARIOS" },
-    { text: "KM 54", value: "KM 54" },
-    { text: "CASA DE TEJAS", value: "CASA DE TEJAS" },
-    { text: "EL PORTILLO", value: "EL PORTILLO" },
-    { text: "POCHOMIL", value: "POCHOMIL" },
-    { text: "EL CHILAMATE", value: "EL CHILAMATE" },
-    { text: "GUTIERREZ NORTE", value: "GUTIERREZ NORTE" },
-    { text: "SAN CRISTOBAL", value: "SAN CRISTOBAL" },
-    { text: "SAN JOSE DE LA MONTAÑA", value: "SAN JOSE DE LA MONTAÑA" },
-    { text: "LOS VELASQUEZ", value: "LOS VELASQUEZ" },
-    { text: "LOS ANGULO", value: "LOS ANGULO" },
-    { text: "SANTA RITA", value: "SANTA RITA" },
-    { text: "LOS BALTODANO", value: "LOS BALTODANO" },
-    { text: "CITALAPA", value: "CITALAPA" },
-    { text: "GUTIERREZ SUR", value: "GUTIERREZ SUR" },
-    { text: "SANCHEZ NORTE", value: "SANCHEZ NORTE" },
-    { text: "SANCHEZ SUR", value: "SANCHEZ SUR" },
-    { text: "LOS GARCIA", value: "LOS GARCIA" },
-    { text: "LOS VIVAS", value: "LOS VIVAS" },
-    { text: "LA GALLINA", value: "LA GALLINA" },
+  MANAGUA: [{ text: 'SIN INFORMACIÓN', value: 'SIN INFORMACIÓN' }],
+  'SAN RAFAEL DEL SUR': [
+    { text: 'SAN RAFAEL DEL SUR', value: 'SAN RAFAEL DEL SUR' },
+    { text: 'EL SALTO', value: 'EL SALTO' },
+    { text: 'LOS RIZO', value: 'LOS RIZO' },
+    { text: 'MASACHAPA', value: 'MASACHAPA' },
+    { text: 'SAN PABLO', value: 'SAN PABLO' },
+    { text: 'LOS LARIOS', value: 'LOS LARIOS' },
+    { text: 'KM 54', value: 'KM 54' },
+    { text: 'CASA DE TEJAS', value: 'CASA DE TEJAS' },
+    { text: 'EL PORTILLO', value: 'EL PORTILLO' },
+    { text: 'POCHOMIL', value: 'POCHOMIL' },
+    { text: 'EL CHILAMATE', value: 'EL CHILAMATE' },
+    { text: 'GUTIERREZ NORTE', value: 'GUTIERREZ NORTE' },
+    { text: 'SAN CRISTOBAL', value: 'SAN CRISTOBAL' },
+    { text: 'SAN JOSE DE LA MONTAÑA', value: 'SAN JOSE DE LA MONTAÑA' },
+    { text: 'LOS VELASQUEZ', value: 'LOS VELASQUEZ' },
+    { text: 'LOS ANGULO', value: 'LOS ANGULO' },
+    { text: 'SANTA RITA', value: 'SANTA RITA' },
+    { text: 'LOS BALTODANO', value: 'LOS BALTODANO' },
+    { text: 'CITALAPA', value: 'CITALAPA' },
+    { text: 'GUTIERREZ SUR', value: 'GUTIERREZ SUR' },
+    { text: 'SANCHEZ NORTE', value: 'SANCHEZ NORTE' },
+    { text: 'SANCHEZ SUR', value: 'SANCHEZ SUR' },
+    { text: 'LOS GARCIA', value: 'LOS GARCIA' },
+    { text: 'LOS VIVAS', value: 'LOS VIVAS' },
+    { text: 'LA GALLINA', value: 'LA GALLINA' }
   ],
-  JINOTEPE: [{ text: "SIN INFORMACIÓN", value: "SIN INFORMACIÓN" }],
-  MASATEPE: [{ text: "SIN INFOMACION", value: "SIN INFORMACIÓN" }],
-};
+  JINOTEPE: [{ text: 'SIN INFORMACIÓN', value: 'SIN INFORMACIÓN' }],
+  MASATEPE: [{ text: 'SIN INFOMACION', value: 'SIN INFORMACIÓN' }]
+}
 //redes sociales
-export const rsociales = [
-  "INSTAGRAM",
-  "YOUTUBE",
-  "LINKEDLN",
-  "TIKTOK",
-  "QUORA",
-];
+export const rsociales = ['INSTAGRAM', 'YOUTUBE', 'LINKEDLN', 'TIKTOK', 'QUORA']
 //actividades economicas
 export const aeconomica = [
-  "COMERCIO",
-  "SERVICIO",
-  "PEQ. INDUSTRIA",
-  "AGROPECUARIO",
-  "PESCA",
-  "OTRO",
-];
+  'AGRICOLA',
+  'COMERCIO',
+  'SERVICIO',
+  'PEQ. INDUSTRIA',
+  'PECUARIO O GANADERO',
+  'PESCA',
+  'OTRO'
+]
 // LOAN DETAILS-> MONTH OF PAYMENT
 export const DatosPrestamo = [
-  "1 MESES",
-  "2 MESES",
-  "3 MESES",
-  "4 MESES",
-  "5 MESES",
-  "6 MESES",
-  "7 MESES",
-  "8 MESES",
-  "9 MESES",
-  "10 MESES",
-  "11 MESES",
-  "12 MESES",
-  "13 MESES",
-  "14 MESES",
-  "15 MESES",
-  "16 MESES",
-  "17 MESES",
-  "18 MESES",
-  "19 MESES",
-  "20 MESES",
-  "21 MESES",
-  "22 MESES",
-  "23 MESES",
-  "24 MESES",
-];
+  '1 MESES',
+  '2 MESES',
+  '3 MESES',
+  '4 MESES',
+  '5 MESES',
+  '6 MESES',
+  '7 MESES',
+  '8 MESES',
+  '9 MESES',
+  '10 MESES',
+  '11 MESES',
+  '12 MESES',
+  '13 MESES',
+  '14 MESES',
+  '15 MESES',
+  '16 MESES',
+  '17 MESES',
+  '18 MESES',
+  '19 MESES',
+  '20 MESES',
+  '21 MESES',
+  '22 MESES',
+  '23 MESES',
+  '24 MESES'
+]
 
 //UNIT INFORMATION
 export const UnitM = [
-  "UNIDAD",
-  "GALON",
-  "BOLSON",
-  "DOCENA",
-  "QUINTAL",
-  "SACO",
-  "CAJA",
-  "FLETE",
-  "MARCA",
-  "PULGADAS",
-  "LIBRAS",
-  "METROS",
-];
+  'ARROBA',
+  'BIDON',
+  'BOLSON',
+  'CAJA',
+  'CAJILLA',
+  'CIEN',
+  'DOCENA',
+  'FLETE',
+  'GALON',
+  'LIBRAS',
+  'LITROS',
+  'MANZANA',
+  'MARCA',
+  'METROS',
+  'MIL',
+  'PACA',
+  'PAQUETE',
+  'PULGADAS',
+  'QUINTAL',
+  'SACO',
+  'UNIDAD'
+]
 
 //api cloudinary
-const CLOUDINARY_UPLOAD_PRESET = "bwhefoph";
+const CLOUDINARY_UPLOAD_PRESET = 'bwhefoph'
 const apiClient = axios.create({
-  baseURL: "https://api.cloudinary.com/v1_1/dptw3ohtb/image/upload",
+  baseURL: 'https://api.cloudinary.com/v1_1/dptw3ohtb/image/upload',
   withCredentials: false,
   headers: {
-    Accept: "multipart/form-data",
-    "Content-Type": "multipart/form-data",
-  },
-});
+    Accept: 'multipart/form-data',
+    'Content-Type': 'multipart/form-data'
+  }
+})
 
 //api to connect phpmailer
 const apiSendMail = axios.create({
   baseURL: SERVER_INFO.serverPame,
-  withCredentials: false,
-});
+  withCredentials: false
+})
 //api error mail
 const apiErrMail = axios.create({
   baseURL: SERVER_INFO.serverPameError,
-  withCredentials: false,
-});
+  withCredentials: false
+})
 
 const config = {
   onUploadProgress: (event) => {
-    store.commit("updateProgress", (event.loaded * 100) / event.total);
-  },
-};
+    store.commit('updateProgress', (event.loaded * 100) / event.total)
+  }
+}
 
 export default {
   async uploadId(inputId, image) {
-    const file = image;
-    store.commit(`${inputId}updateProgress`, 0);
+    const file = image
+    store.commit(`${inputId}updateProgress`, 0)
     if (!file) {
-      return;
+      return
     }
     const options = {
       maxSizeMB: 1,
       maxWidthOrHeight: 450,
       useWebWorker: true,
       onProgress: (progress) => {
-        store.commit(`${inputId}updateProgress`, progress);
-      },
-    };
+        store.commit(`${inputId}updateProgress`, progress)
+      }
+    }
     try {
-      const compressFile = imageCompression(file, options);
+      const compressFile = imageCompression(file, options)
       //const formData = new FormData();
       // formData.append("file", await compressFile);
       // formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
       // return apiClient.post("", formData);
-      return await compressFile;
+      return await compressFile
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   },
   getBase64ImageFromURL(url) {
     return new Promise((resolve, reject) => {
-      var img = new Image();
-      img.setAttribute("crossOrigin", "anonymous");
+      var img = new Image()
+      img.setAttribute('crossOrigin', 'anonymous')
 
       img.onload = () => {
-        var canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
+        var canvas = document.createElement('canvas')
+        canvas.width = img.width
+        canvas.height = img.height
 
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
+        var ctx = canvas.getContext('2d')
+        ctx.drawImage(img, 0, 0)
 
-        var dataURL = canvas.toDataURL("image/png");
+        var dataURL = canvas.toDataURL('image/png')
 
-        resolve(dataURL);
-      };
+        resolve(dataURL)
+      }
 
       img.onerror = (error) => {
-        reject(error);
-      };
+        reject(error)
+      }
 
-      img.src = url;
-    });
-  },
-};
+      img.src = url
+    })
+  }
+}
 
 export const sendMail = ({ name, ncedula, email, data }) => {
-  return apiSendMail.post("", {
+  return apiSendMail.post('', {
     email,
     name,
     ncedula,
-    message: "Adjunto, Nueva Solicitud de Credito de : ",
-    pdf: data,
-  });
-};
+    message: 'Adjunto, Nueva Solicitud de Credito de : ',
+    pdf: data
+  })
+}
 export const errMail = (error) => {
-  return apiErrMail.post("", error);
-};
+  return apiErrMail.post('', error)
+}
 
 //currency method
 export const money = (value) =>
-  new Intl.NumberFormat("es-NI", {
-    currency: "NIO",
-    style: "currency",
-  }).format(value);
+  new Intl.NumberFormat('es-NI', {
+    currency: 'NIO',
+    style: 'currency'
+  }).format(value)
 
 // made table
 export const renderTable = (data, columns, total, tname) => {
   return {
-    pageBreak: "after",
+    pageBreak: 'after',
     table: {
       headerRows: 1,
-      widths: ["*", "auto", "auto", "auto", "auto"],
-      body: buildTableHeader(data, columns, total, tname),
-    },
-  };
-};
+      widths: ['*', 'auto', 'auto', 'auto', 'auto'],
+      body: buildTableHeader(data, columns, total, tname)
+    }
+  }
+}
 export const renderTableG = (data, columns, total, tname) => {
   return {
     table: {
       headerRows: 1,
       widths: [
-        "auto",
-        "auto",
-        "auto",
-        "auto",
-        "auto",
-        "auto",
-        "auto",
-        "auto",
-        "*",
+        'auto',
+        'auto',
+        'auto',
+        'auto',
+        'auto',
+        'auto',
+        'auto',
+        'auto',
+        '*'
       ],
-      body: buildTableHeader(data, columns, total, tname),
-    },
-  };
-};
+      body: buildTableHeader(data, columns, total, tname)
+    }
+  }
+}
 
 //build header pdf
 const buildTableHeader = (data, columns, total, tname) => {
-  if (tname === "tProducto") {
-    let body = [];
+  if (tname === 'tProducto') {
+    let body = []
     body.push([
       {
-        text: "PROGRAMA DE APOYO A MICROEMPRESARIOS.",
+        text: 'PROGRAMA DE APOYO A MICROEMPRESARIOS.',
         colSpan: 5,
         margin: 5,
-        alignment: "center",
+        alignment: 'center'
       },
-      "",
-      "",
-      "",
-      "",
-    ]);
+      '',
+      '',
+      '',
+      ''
+    ])
     body.push([
       {
-        text: "Plan De Inversión.",
+        text: 'Plan De Inversión.',
         colSpan: 5,
         margin: 5,
-        alignment: "center",
+        alignment: 'center'
       },
-      "",
-      "",
-      "",
-      "",
-    ]);
-    buildTableBody(data, columns, body);
+      '',
+      '',
+      '',
+      ''
+    ])
+    buildTableBody(data, columns, body)
     body.push([
       {
-        text: "Total",
-        alignment: "right",
-        colSpan: 4,
+        text: 'Total',
+        alignment: 'right',
+        colSpan: 4
       },
-      "",
-      "",
-      "",
-      { text: `${money(total)}`, alignment: "center" },
-    ]);
-    return body;
+      '',
+      '',
+      '',
+      { text: `${money(total)}`, alignment: 'center' }
+    ])
+    return body
   } else {
-    let body = [];
+    let body = []
     body.push([
       {
-        text: "PROGRAMA DE APOYO A MICROEMPRESARIOS.",
+        text: 'PROGRAMA DE APOYO A MICROEMPRESARIOS.',
         colSpan: 9,
         margin: 5,
-        alignment: "center",
+        alignment: 'center'
       },
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-    ]);
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      ''
+    ])
     body.push([
       {
-        text: "Garantías Prendarias.",
+        text: 'Garantías Prendarias.',
         colSpan: 9,
         margin: 5,
-        alignment: "center",
+        alignment: 'center'
       },
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-    ]);
-    buildTableBody(data, columns, body);
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      ''
+    ])
+    buildTableBody(data, columns, body)
     body.push([
       {
-        text: "Total",
-        alignment: "right",
-        colSpan: 7,
+        text: 'Total',
+        alignment: 'right',
+        colSpan: 7
       },
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      { text: `${money(total)}`, alignment: "center" },
-      { text: "C$", alignment: "left" },
-    ]);
-    return body;
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      { text: `${money(total)}`, alignment: 'center' },
+      { text: 'C$', alignment: 'left' }
+    ])
+    return body
   }
-};
+}
 //build body table
 const buildTableBody = (data, columns, body) => {
-  let header = [];
-  columns.forEach((e) => header.push({ text: `${e}`, alignment: "center" }));
-  body.push(header);
+  let header = []
+  columns.forEach((e) => header.push({ text: `${e}`, alignment: 'center' }))
+  body.push(header)
   data.forEach(function (row) {
-    var dataRow = [];
+    var dataRow = []
     columns.forEach(function (column) {
       dataRow.push({
         text: `${row[column].toString()}`,
-        alignment: "center",
-      });
-    });
-    body.push(dataRow);
-  });
-};
+        alignment: 'center'
+      })
+    })
+    body.push(dataRow)
+  })
+}
 
 //Capitalize first letter of each word
 export const toTitleCase = (phrase) => {
   return phrase
     .toLowerCase()
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-};
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+}
